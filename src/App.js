514bb75676319e945f9978561c037e9228a0b129
Andres Velazquez <jvandres247@gmail.com>
2021-02-23 11:14:36 -0600

    add material-ui and react json view

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Button from '@material-ui/core/Button';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { Grid, Container } from "@material-ui/core";
+import ChartInfo from "./components/ChartInfo";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    paddingTop: 50,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+  },
+}));
 
 function App() {
+  const numbers = [1, 2, 3, 4, 5, 6];
+  const classes = useStyles();
+
   return (
-    <Button variant="contained" color="primary">
-      Hola Mundo!
-    </Button>
+    <Container className={classes.root} maxWidth="lg">
+      <Grid container spacing={3}>
+        {numbers.map((number) => (
+          <Grid item xs={4} key={number}>
+            <ChartInfo />
+          </Grid>
+        ))}
+      </Grid>
+    </Container>
   );
 }
 
